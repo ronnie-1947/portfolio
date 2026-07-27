b01788f38c7e72300cbd4f7f8dfebd32be869093
Ripunjoy <ripunjoy.buddha@gmail.com>
2026-07-26 23:30:11 -0400

    updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
+// @ts-ignore
 import "./globals.css";
 
 const geistSans = Geist({
@@ -20,15 +20,13 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
   icons: {
     icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon_io/favicon.ico", sizes: "any" },
+      { url: "/favicon_io/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon_io/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon_io/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/favicon_io/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
     ],
-  },
-  other: {
-    "msapplication-TileColor": "#ffffff",
-    "msapplication-TileImage": "/ms-icon-144x144.png",
-    "msapplication-config": "/browserconfig.xml",
+    apple: [{ url: "/favicon_io/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
   },
 };
 
